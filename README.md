ea7662c9a6dae2f432c50efda9b98a6fd3c481b1
lance0512 <post.thinking@gmail.com>
2018-07-24 11:24:59 +0800

    修改了readme.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # node-backend-services
+
+### 开发模式
+`npm run dev`
+
+### 单元测试
+`npm run test`
+
+### 测试覆盖报告
+`npm run cov`
+
--- app/controller/home.js
+++ app/controller/home.js
@@ -2,12 +2,12 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-	 //app.js启动时获取的天气数据
-    console.log(this.ctx.app.cities)
-	this.ctx.body = this.ctx.app.cities;
-	
-  }
+    async index() {
+        //app.js启动时获取的天气数据
+        console.log(this.ctx.app.cities)
+        this.ctx.body = this.ctx.app.cities;
+
+    }
 }
 
 module.exports = HomeController;
