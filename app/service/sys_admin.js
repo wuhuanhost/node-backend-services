798f381967948f85bb08a48c3b1c79255aeee6eb
wuhuanhost <post.thinking@gmail.com>
2019-06-21 17:43:34 +0800

    '修改了系统用户管理'

--- app/service/sys_admin.js
+++ app/service/sys_admin.js
@@ -12,7 +12,7 @@ class SysAdmin extends Service {
 	 * @param 密码 md5Pwd
 	 */
 	async login(account, md5Pwd) {
-		var admin = await this.ctx.model.Admin.findOne({
+		let admin = await this.ctx.model.Admin.findOne({
 			where: {
 				account: account,
 				password: md5Pwd
@@ -29,8 +29,21 @@ class SysAdmin extends Service {
 	}
 	//修改用户
 	async updateAdmin(admin) {}
-	//分页查询用户列表
-	async queryAdminsByPage(startNo, pageSize) {}
+	/**
+	 * 分页查询用户列表
+	 * ! 是第一页还是最后一页由前端去计算和判断，后端只返回一个满足条件的总页数，每页显示的条数前端存在配置文件中
+	 * @param  显示页数         pageNum
+	 * @param  每页显示的条数    pageSize
+	 */
+	async queryAdminsByPage(pageNum, pageSize) {
+		let admin = await this.ctx.model.Admin.findAndCountAll({
+			where: {
+				status: 1
+			},
+			limit: 0, //起始
+			offset: pageSize //显示的条数
+		});
+	}
 	// 根据用户查询用户信息
 	async queryAdminById(adminId) {}
 	// 删除账号(物理删除)
